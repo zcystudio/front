ed351fa788c3d59c17b7e1f293315356d8c2028a
zhaochy <zhaochy_2015@hotmail.com>
2019-03-07 17:02:57 +0800

    fix eslint bugs
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Link from 'next/link';
 
 const Index = () => (
   <div>
     <Link href="/about">
-      <button>Go to About Page</button>
+      <button type="button">Go to About Page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
-)
+);
 
 export default Index;
