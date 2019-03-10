c6e42a5af65b46764fc2023450a1bc99c31c9a2a
Zhao Chaoyi <zhaochy_2015@hotmail.com>
2019-03-10 19:59:10 +0800

    Cd (#4)
    setup jenkins CD
    add dingTalk webhook
    
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- jenkins_scripts/build.sh
+++ jenkins_scripts/build.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 
-echo 'build docker image'
+dockercleanrepo() {
+  docker images --no-trunc --format '{{.ID}} {{.Repository}}' \
+  | grep "front" | awk '{print $1}' | xargs -t docker rmi -f
+}
+
+echo '########  start build docker image   ##########'
 
 # timestamp: year-month-day-hour-minute
 time=$(date "+%Y%m%d-%H%M")
 
-# current dir
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 echo 'current time:' $time
 
-docker build --no-cache -t front:$time $dir/..
+pwd
+echo $dir
+
+echo 'Clean Front Docker Repository'
+dockercleanrepo
+
+docker build --no-cache -t front .
+
+echo '#### Dump Docker image and clean build artifacts ####'
+docker images
+
+echo 'Remove Intermediate Build Images'
+docker rmi $(docker images --filter "dangling=true" -q)
+
+echo 'Dump Docker image'
+docker save --output front.tar front
+
+echo 'Clean Front Docker Repository'
+dockercleanrepo
+
+ls -al --block-size=M
+
+
---
+++ jenkins_scripts/dingTalk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+url='https://oapi.dingtalk.com/robot/send?access_token=0bdc432c1e8214ca0a7d1717c0b7ddea1368c15217bc4778f79b4621cc2af395';
+author=$(git show -s --pretty=%an)
+
+content="$author 在 $GIT_BRANCH 分支的 $1 成功啦 $BUILD_URL"
+
+echo "$content"
+
+generate_post_data(){
+  cat <<EOF
+{
+  "msgtype": "text",
+  "text": {
+    "content": "$content"
+  }
+}
+EOF
+}
+
+curl --request POST \
+  --header "Content-Type: application/json" \
+  --data "$(generate_post_data)" \
+  $url
+
+
