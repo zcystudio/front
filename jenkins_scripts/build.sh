fa1157e564f064e24c49c84e7dffe9623475b241
zhaochy <zhaochy_2015@hotmail.com>
2019-03-10 11:15:01 +0800

    fix jenkins build script
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- jenkins_scripts/build.sh
+++ jenkins_scripts/build.sh
@@ -5,7 +5,10 @@ echo 'build docker image'
 # timestamp: year-month-day-hour-minute
 time=$(date "+%Y%m%d-%H%M")
 
+# current dir
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo 'current time:' $time
 
 cd ..
-docker build --no-cache -t front:$time .
+docker build --no-cache -t front:$time $dir/..
