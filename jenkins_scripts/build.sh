1ff00a515034235bbd9c4c0ccefa0f3914821c76
zhaochy <zhaochy_2015@hotmail.com>
2019-03-10 12:01:48 +0800

    update jenkins scripts
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- jenkins_scripts/build.sh
+++ jenkins_scripts/build.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-echo 'build docker image'
+set -e
+
+echo '########  start build docker image   ##########'
 
 # timestamp: year-month-day-hour-minute
 time=$(date "+%Y%m%d-%H%M")
 
-# current dir
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 echo 'current time:' $time
 
-docker build --no-cache -t front:$time $dir/..
+pwd
+echo $dir
+
+docker build --no-cache -t front:$time .
+
+echo '#### Dump Docker image and clean build artifacts ####'
+
+docker images
+
+echo 'Remove Intermediate Build Images'
+
+docker rmi $(docker images --filter "dangling=true" -q)
+
+echo 'Dump Docker image'
+
+docker save --output front.$time.tar front:$time
+
+ls -al --block-size=M
