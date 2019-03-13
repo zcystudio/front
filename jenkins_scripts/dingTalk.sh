301a57b320b65454cdcf8e1c94cf5a1191e61099
zhaochy <zhaochy_2015@hotmail.com>
2019-03-13 17:28:35 +0800

    update dingTalk message format
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

--- jenkins_scripts/dingTalk.sh
+++ jenkins_scripts/dingTalk.sh
@@ -5,7 +5,10 @@ set -e
 url='https://oapi.dingtalk.com/robot/send?access_token=0bdc432c1e8214ca0a7d1717c0b7ddea1368c15217bc4778f79b4621cc2af395';
 author=$(git show -s --pretty=%an)
 
-content="$author 在 $GIT_BRANCH 分支的 $1 成功啦 $BUILD_URL"
+content="""$JOB_NAME 由 $author 在 $GIT_BRANCH 分支的 $1 成功啦
+====================
+$BUILD_URL
+"""
 
 echo "$content"
 
