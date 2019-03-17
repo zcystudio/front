87512cee85ff97f6e146e14ea9fed8b75f146bb4
Cold-Meteor <xiaoyu09@mail.ustc.edu.cn>
2019-03-17 22:03:01 +0800

    add express2 Signed-off-by: Cold-Meteor <xiaoyu09@mail.ustc.edu.cn>

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const next = require('next');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const compression = require('compression');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -7,6 +12,14 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  // morgan
+  server.use(morgan('tiny'));
+  // cookie-parser
+  server.use(cookieParser());
+  // use the session middleware
+  server.use(session({ secret: 'password', cookie: { masAge: 60000 } }));
+  // compress responses
+  server.use(compression());
   server.get('*', (req, res) => {
     handle(req, res);
   });
